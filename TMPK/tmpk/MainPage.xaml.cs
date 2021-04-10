162be0ea37274b67c5b2dc504eb4876580f676b3
kirill <kirill@DESKTOP-D4NV2EJ>
2021-04-10 23:01:13 +0300

    ааа

--- TMPK/tmpk/MainPage.xaml.cs
+++ TMPK/tmpk/MainPage.xaml.cs
@@ -4,15 +4,54 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tmpk.functsii;
 using Xamarin.Forms;
 
 namespace tmpk
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            tmpk.ItemsSource = Menulist();
+            var StartPage = typeof(stranitsi.osnova);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(StartPage));
+            IsPresented = false;
+        }
+
+        private List<klas> Menulist()
+        {
+            var spisok = new List<klas>();
+            spisok.Add(new klas()
+            {
+                Text = "Расписание",
+                ImageP = "pilt.png",
+                TarPage = typeof(stranitsi.raspisanie)
+            });
+
+            spisok.Add(new klas()
+            {
+                Text = "Замены",
+                ImageP = "pilt.png",
+                TarPage = typeof(stranitsi.zameni)
+            });
+
+            spisok.Add(new klas()
+            {
+                Text = "Инфо.",
+                ImageP = "pilt.png",
+                TarPage = typeof(stranitsi.osnova)
+            });
+            return spisok;
+        }
+
+        private void tmpk_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selecte = (klas)e.SelectedItem;
+            Type selectedpage = selecte.TarPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedpage));
+            IsPresented = false;
         }
     }
 }
---
+++ TMPK/tmpk/functsii/classs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tmpk.functsii
+{
+    public class classs
+    {
+        public string Ltext { get; set; }
+
+        public string lDetail { get; set; }
+
+        public string Limage { get; set; }
+
+        public Type TarPage { get; set; }
+    }
+}
